718b418abca4cc39ab37c8cf38be4470fad1fc63
mikhe <mikheeva@1454.ru>
2021-01-13 13:47:15 +0300

    HW_1

---
+++ app/src/main/java/com/example/android_kotlin/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.android_kotlin
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+       
+    }
+}
